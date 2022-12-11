f152c6a0197d9c8fd5bfd3222a55a7cf2ef74453
shoaib <shoaib100aib@gmail.com>
2022-12-11 11:46:57 +0530

    first commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,78 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import {
+  Image,
+  StyleSheet,
+  Text,
+  TextInput,
+  TextInputComponent,
+  TouchableOpacity,
+  View,
+} from "react-native";
 
-export default function App() {
+const App = () => {
+  const [todo, settodo] = useState("");
+  const [todos, settodos] = useState([]);
+  const handleAddTodo  =() =>{
+
+    if(!todo)return
+
+
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.heading}>{todo}</Text>
+      <View style={styles.inputContainer}>
+        <TextInput
+          onChangeText={(text) => settodo(text)}
+          value={todo}
+          placeholder="Enter a text "
+          style={styles.input}
+        />
+        <TouchableOpacity onPress={handleAddTodo}>
+          <Text style={styles.button}>Go</Text>
+        </TouchableOpacity>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
-}
+};
+export default App;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#dad7cd",
+    alignItems: "center",
+    // justifyContent: "center",
+  },
+  heading: {
+    paddingTop: 75,
+    marginVertical: 15,
+    fontSize: 35,
+
+    fontWeight: "500",
+  },
+  inputContainer: {
+    flexDirection: "row",
+    marginHorizontal: 10,
+    alignItems: "center",
+  },
+  input: {
+    flex: 1,
+    shadowColor: "black",
+    backgroundColor: "#a3b18a",
+    elevation: 10,
+    marginRight: 5,
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    borderRadius: 40,
+  },
+  button: {
+    padding: 13,
+    color: "white",
+    backgroundColor: "#344e41",
+    borderRadius: 50,
+    elevation: 10,
   },
 });
---
+++ demo.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const demo = () => {
+  return (
+    <View>
+      <Text>demo</Text>
+    </View>
+  )
+}
+
+export default demo
+
+const styles = StyleSheet.create({})
